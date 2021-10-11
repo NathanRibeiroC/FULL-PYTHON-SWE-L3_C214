f68dcb3f55ea85b3d81cc1b7c616f7b42b2b10af
NathanRibeiroC <nathanazevedo@gec.inatel.br>
2021-10-11 10:17:03 -0300

    feature: call ui

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.view.interface import *
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
