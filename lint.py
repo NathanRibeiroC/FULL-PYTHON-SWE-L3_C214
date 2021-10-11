6918bd45d1414192695d6411ebadca5956f7231d
NathanRibeiroC <nathanazevedo@gec.inatel.br>
2021-10-11 19:43:08 -0300

    refactor: lint

--- lint.py
+++ lint.py
@@ -1,54 +1,13 @@
-import argparse
-import logging
-from pylint.lint import Run
+import sys
+from pylint import lint
 
+THRESHOLD = 9
 
-logging.getLogger().setLevel(logging.INFO)
+if len(sys.argv) < 2:
+    raise ArgumentError("Module to evaluate needs to be the first argument")
 
-parser = argparse.ArgumentParser(prog="LINT")
+run = lint.Run([sys.argv[1:]], do_exit=False)
+score = run.linter.stats['global_note'] # Yes this is a terrible name for the score
 
-parser.add_argument('-p',
-                    '--path',
-                    help='path to directory you want to run pylint | '
-                         'Default: %(default)s | '
-                         'Type: %(type)s ',
-                    default='./src',
-                    type=str)
-
-parser.add_argument('-t',
-                    '--threshold',
-                    help='score threshold to fail pylint runner | '
-                         'Default: %(default)s | '
-                         'Type: %(type)s ',
-                    default=7,
-                    type=float)
-
-args = parser.parse_args()
-path = str(args.path)
-threshold = float(args.threshold)
-
-logging.info('PyLint Starting | '
-             'Path: {} | '
-             'Threshold: {} '.format(path, threshold))
-
-results = Run([path], do_exit=False)
-
-final_score = results.linter.stats['global_note']
-
-if final_score < threshold:
-
-    message = ('PyLint Failed | '
-               'Score: {} | '
-               'Threshold: {} '.format(final_score, threshold))
-
-    logging.error(message)
-    raise Exception(message)
-
-else:
-    message = ('PyLint Passed | '
-               'Score: {} | '
-               'Threshold: {} '.format(final_score, threshold))
-
-    logging.info(message)
-
-    exit(0)
+if score < THRESHOLD:
+    sys.exit(1)
